bec8da00bd3507448425702165073e26e0613250
MrMM1989 <maarten_89@msn.com>
2016-02-14 23:03:30 +0100

    Homepage met shared header en footer

--- code/vendor/mmprogramming/mmparcs/src/View/Home/Index.php
+++ code/vendor/mmprogramming/mmparcs/src/View/Home/Index.php
@@ -1,37 +1,4 @@
-<header>
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="index.php">MMParcs</a>
-			</div>
-			<div id="navbar" class="collapse navbar-collapse">
-				<ul class="nav navbar-nav navbar-left">
-					<li>
-						<a href="index.php">Praktische Info</a>
-					</li>
-					<li>
-						<a href="index.php">Gastenboek</a>
-					</li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li>
-						<a href="index.php"><i class="fa fa-sign-in"></i> Inloggen</a>
-					</li>
-					<li>
-						<a href="index.php"><i class="fa fa-user-plus"></i> Registreren</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-</header>
-
+<?php $partialView('Shared', 'Header'); ?>
 <div id="pbody" class="container">
 	<div class="row contentrow">
 		<div class="col-sm-4">
@@ -68,32 +35,4 @@
 		</div>		
 	</div>
 </div>
-
-<footer>
-	<div class="container">
-		<div class="row footerline">
-			<div class="col-sm-4">
-				<p>
-					<a href="index.php">Algemene Voorwaarden</a>
-				</p>
-			</div>
-			<div class="col-sm-4">
-				<p class="align-center">
-					<a href="index.php">Privacybeleid</a>
-				</p>
-			</div>
-			<div class="col-sm-4">
-				<p class="align-right">
-					<a href="index.php">FAQ</a>
-				</p>
-			</div>
-		</div>
-		<div class="row footerline">
-			<div class="col-sm-12">
-				<p class="align-center">
-					<a href="index.php">Administratorpaneel</a>
-				</p>
-			</div>
-		</div>
-	</div>
-</footer>
+<?php $partialView('Shared', 'Footer'); ?>
---
+++ code/vendor/mmprogramming/mmparcs/src/View/Shared/Footer.php
@@ -0,0 +1,28 @@
+<footer>
+	<div class="container">
+		<div class="row footerline">
+			<div class="col-sm-4">
+				<p>
+					<a href="index.php">Algemene Voorwaarden</a>
+				</p>
+			</div>
+			<div class="col-sm-4">
+				<p class="align-center">
+					<a href="index.php">Privacybeleid</a>
+				</p>
+			</div>
+			<div class="col-sm-4">
+				<p class="align-right">
+					<a href="index.php">FAQ</a>
+				</p>
+			</div>
+		</div>
+		<div class="row footerline">
+			<div class="col-sm-12">
+				<p class="align-center">
+					<a href="index.php">Administratorpaneel</a>
+				</p>
+			</div>
+		</div>
+	</div>
+</footer>
