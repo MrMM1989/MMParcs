193d19e6f164bca1c4cfffd5c8259fb58992acd5
MrMM1989 <maarten_89@msn.com>
2016-02-14 22:53:24 +0100

    Homepagina HTML + CSS

--- code/index.php
+++ code/index.php
@@ -24,6 +24,7 @@ $view = $routeConfig->invokeActionMethod();
     <title><?php echo $route->getEntity().'-'.$route->getAction(); ?> | MMParcs</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
     integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="css/site.css" />
     </head>
 <body>
--- code/vendor/mmprogramming/mmparcs/src/View/Home/Index.php
+++ code/vendor/mmprogramming/mmparcs/src/View/Home/Index.php
@@ -1,3 +1,99 @@
-<div>
-	<p>Dit is de homepage!</p>
+<header>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.php">MMParcs</a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-left">
+					<li>
+						<a href="index.php">Praktische Info</a>
+					</li>
+					<li>
+						<a href="index.php">Gastenboek</a>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<a href="index.php"><i class="fa fa-sign-in"></i> Inloggen</a>
+					</li>
+					<li>
+						<a href="index.php"><i class="fa fa-user-plus"></i> Registreren</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
+
+<div id="pbody" class="container">
+	<div class="row contentrow">
+		<div class="col-sm-4">
+			<h3>Attracties</h3>
+			<p>Er is voor iedereen iets te beleven in dit park! Van rustige tot echte thrillrides... we hebben het allemaal!</p>
+			<p><a href="index.php">Ontdek attracties</a></p>
+		</div>
+		<div class="col-sm-4">
+			<h3>MMBad</h3>
+			<p>Neem een duik in het MMBad, het leukste subtropisch zwemparadijs!</p>
+			<p><a href="index.php">Plons in het MMBad</a></p>
+		</div>
+		<div class="col-sm-4">
+			<h3>Vakantiepark</h3>
+			<p>De kans is groot dat je niet alles op één dag kan doen, onze comfortabele huisjes zorgen voor een onvergetelijk verblijf!</p>
+			<p><a href="index.php">Boek nu je verblijf!</a></p>
+		</div>		
+	</div>
+	<div class="row contentrow">
+		<div class="col-sm-4">
+			<h3>Jobs</h3>
+			<p>Werken bij MMParcs? Natuurlijk!</p>
+			<p><a href="index.php">Bekijk alle openstaande vacatures</a></p>
+		</div>
+		<div class="col-sm-4">
+			<h3>Openingsuren</h3>
+			<p>Bereid je bezoek echt goed voor en raadpleeg hier de openingsuren!</p>
+			<p><a href="index.php">De openingsuren</a></p>
+		</div>
+		<div class="col-sm-4">
+			<h3>Tarieven</h3>
+			<p>Benieuwd wat een bezoek je zal kosten? Kijk het hier na!</p>
+			<p><a href="index.php">De tarieven</a></p>
+		</div>		
+	</div>
 </div>
+
+<footer>
+	<div class="container">
+		<div class="row footerline">
+			<div class="col-sm-4">
+				<p>
+					<a href="index.php">Algemene Voorwaarden</a>
+				</p>
+			</div>
+			<div class="col-sm-4">
+				<p class="align-center">
+					<a href="index.php">Privacybeleid</a>
+				</p>
+			</div>
+			<div class="col-sm-4">
+				<p class="align-right">
+					<a href="index.php">FAQ</a>
+				</p>
+			</div>
+		</div>
+		<div class="row footerline">
+			<div class="col-sm-12">
+				<p class="align-center">
+					<a href="index.php">Administratorpaneel</a>
+				</p>
+			</div>
+		</div>
+	</div>
+</footer>
